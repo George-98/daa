86215ae2105daffbcc5da59fb122be9770e9cea5
George Lee <l.georgelee98@hotmail.com>
2026-03-20 15:19:22 +0000

    Initial Commit

---
+++ DAA/DAAApp.swift
@@ -0,0 +1,17 @@
+//
+//  DAAApp.swift
+//  DAA
+//
+//  Created by George Lee on 20/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct DAAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DAATests/DAATests.swift
@@ -0,0 +1,16 @@
+//
+//  DAATests.swift
+//  DAATests
+//
+//  Created by George Lee on 20/03/2026.
+//
+
+import Testing
+
+struct DAATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
